2b7697fef2569fe2723963ff8d93cd69562d6ff9
Christian Diaz Portela <christiandp7@gmail.com>
2021-10-15 04:37:50 -0400

    :lipstick: Setup tailwind config file with theme colors and sizes

--- theme/assets/main.js
+++ theme/assets/main.js
@@ -7,15 +7,16 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/styles/main.css":
 /*!*****************************!*\
   !*** ./src/styles/main.css ***!
   \*****************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nError: Can't resolve './ui/layout.css' in '/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/src/styles'\\n    at finishWithoutResolve (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:293:18)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:362:15\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:16:1)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:27:1)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/DescriptionFilePlugin.js:87:43\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at tryRunOrWebpackError (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4966:12)\\n    at __webpack_require__ (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4923:18)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4994:20\\n    at symbolIterator (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/neo-async/async.js:3485:9)\\n    at done (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/neo-async/async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/Hook.js:18:14)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4901:43\\n    at symbolIterator (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/neo-async/async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nError: Can't resolve './ui/layout.css' in '/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/src/styles'\\n    at finishWithoutResolve (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:293:18)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:362:15\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:16:1)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:27:1)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/DescriptionFilePlugin.js:87:43\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\n    at Object.<anonymous> (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/css-loader/dist/cjs.js!/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/postcss-loader/dist/cjs.js!/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/src/styles/main.css:1:7)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:432:11\\n    at Hook.eval [as call] (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/Hook.js:14:14)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4968:39\\n    at tryRunOrWebpackError (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4966:12)\\n    at __webpack_require__ (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4923:18)\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/webpack/lib/Compilation.js:4994:20\\n    at symbolIterator (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/neo-async/async.js:3485:9)\\n\\nGenerated code for /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/css-loader/dist/cjs.js!/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/postcss-loader/dist/cjs.js!/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/src/styles/main.css\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\\\nError: Can't resolve './ui/layout.css' in '/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/src/styles'\\\\n    at finishWithoutResolve (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:293:18)\\\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:362:15\\\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:16:1)\\\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:27:1)\\\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/DescriptionFilePlugin.js:87:43\\\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\\\n    at eval (eval at create (/home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\\\n    at /home/christiandp7/dev/shopify/themes/future-holidays-cdp7-test/node_modules/enhanced-resolve/lib/Resolver.js:410:5\\\");\");\n\n//# sourceURL=webpack://future-holidays-cdp7-test/./src/styles/main.css?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://future-holidays-cdp7-test/./src/styles/main.css?");
 
 /***/ }),
 
@@ -25,8 +26,7 @@ eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extrac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.css */ \"./src/styles/main.css\");\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_main_css__WEBPACK_IMPORTED_MODULE_0__);\n\n\n\n//# sourceURL=webpack://future-holidays-cdp7-test/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.css */ \"./src/styles/main.css\");\n\n\n\n//# sourceURL=webpack://future-holidays-cdp7-test/./src/index.js?");
 
 /***/ })
 
@@ -57,35 +57,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
