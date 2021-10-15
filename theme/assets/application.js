e5dc7f0dd07ae13e384cc90269c9d755c31e8993
Christian Diaz Portela <christiandp7@gmail.com>
2021-10-15 07:09:57 -0400

    :wrench: Setup cart state using Reactive api

--- theme/assets/application.js
+++ theme/assets/application.js
@@ -1,1 +1,17 @@
 // Put your application javascript here
+const store = Vue.reactive({
+	state: {
+		cart: [],
+	},
+
+	getCart() {
+		axios
+			.get('/cart.js')
+			.then(resp => {
+				this.state.cart.unshift(resp.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	},
+})
